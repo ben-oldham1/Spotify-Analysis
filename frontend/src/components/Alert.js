76fb8d292a81af937f054bbebfb1a5ce9e0bade1
Ben <ben.oldham2003@gmail.com>
2023-05-02 14:32:06 +0100

    Implemented dismissable alert for API errors

--- frontend/src/components/Alert.js
+++ frontend/src/components/Alert.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Alert from 'react-bootstrap/Alert';
+import Button from 'react-bootstrap/Button';
 
-function Alert(props) {
-    return <div className={"alert alert-" + props.type + " w-100"} role="alert">
-    <h4 className="alert-heading">{props.heading}</h4>
-    <p>{props.bodyText}</p>
-  </div>;
+function AlertDismissible(props) {
+
+  // We inherit the apiError and setApiError props from parent, which determines whether the error alert shows
+
+  return (
+    <>
+      <Alert show={props.apiError} variant="warning" className="w-100 mx-auto my-3">
+        <Alert.Heading>{props.headerText}</Alert.Heading>
+        <p>
+          {props.bodyText}
+        </p>
+        <div className="d-flex justify-content-end">
+          <Button onClick={() => props.setApiError(false)} variant="outline-warning">
+            Dismiss
+          </Button>
+        </div>
+      </Alert>
+    </>
+  );
 }
 
-export default Alert;
+export default AlertDismissible;
