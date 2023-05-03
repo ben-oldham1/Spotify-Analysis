1fafef951cf1b9d373683f5b7bf610b1b4a97602
Ben <ben.oldham2003@gmail.com>
2023-05-03 12:13:59 +0100

    Implemented API successfully

---
+++ frontend/src/components/TopTracks.js
@@ -0,0 +1,21 @@
+import Stack from 'react-bootstrap/Stack';
+import Col from 'react-bootstrap/Col';
+
+function TopTracks(props) {
+    return (
+        <>
+            {props.topTracksJson.map(topTrack =>
+                <Col className='col-4 text-center mb-5' key={topTrack.id}>
+                    <Stack>
+                        <div className='mb-3'>
+                            <img src={topTrack.album.images[0].url} className="grow" height='200px'></img>
+                        </div>
+                        <h3 className="text-white">{topTrack.name}</h3>
+                    </Stack>
+                </Col>
+            )}
+        </>
+    );
+}
+
+export default TopTracks;
