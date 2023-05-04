81a763d5ba198e1b8664c91d3e72bd41d0607619
Ben <ben.oldham2003@gmail.com>
2023-05-04 13:21:36 +0100

    Included data from getTopGenres to display

--- frontend/src/components/GenreChart.js
+++ frontend/src/components/GenreChart.js
@@ -19,34 +19,68 @@
 // THE SOFTWARE.
 
 // import '../node_modules/react-vis/dist/style.css';
-import {RadialChart} from 'react-vis';
+import { RadialChart } from 'react-vis';
 
 import React from 'react';
-import {format} from 'd3-format';
+import { format } from 'd3-format';
 
 // import RadarChart from 'radar-chart';
 
-export default function SimpleRadialChart(props) {
-    return (
-      <RadialChart
-        colorType={'literal'}
-        colorDomain={[0, 100]}
-        colorRange={[0, 10]}
-        margin={{top: 100}}
-        getLabel={d => d.name}
-        data={[
-          {angle: 1, color: '#89DAC1', name: 'green', opacity: 0.2},
-          {angle: 2, color: '#F6D18A', name: 'yellow'},
-          {angle: 5, color: '#1E96BE', name: 'cyan'},
-          {angle: 3, color: '#DA70BF', name: 'magenta'},
-          {angle: 5, color: '#F6D18A', name: 'yellow again'}
-        ]}
-        labelsRadiusMultiplier={1.1}
-        labelsStyle={{fontSize: 16, fill: '#222'}}
-        showLabels
-        style={{stroke: '#fff', strokeWidth: 2}}
-        width={500}
-        height={400}
-      />
-    );
+export default function GenreChart(props) {
+
+  const topGenreJson = props.topGenreJson;
+
+  // Create an empty object to store the genre counts
+  let genreCounts = {};
+
+  // Loop through each artist in the topGenreJson variable
+  for (let i = 0; i < topGenreJson.length; i++) {
+    // Loop through each genre in the artist's genres array
+    for (let j = 0; j < topGenreJson[i].genres.length; j++) {
+      // If the genre is not in the genreCounts object, add it with a count of 1
+      if (!(topGenreJson[i].genres[j] in genreCounts)) {
+        genreCounts[topGenreJson[i].genres[j]] = 1;
+      }
+      // If the genre is already in the genreCounts object, increment its count
+      else {
+        genreCounts[topGenreJson[i].genres[j]] += 1;
+      }
+    }
   }
+
+
+  // Output the chartData array in the specified format
+  console.log('genreCounts');
+  console.log(genreCounts);
+
+  let genreNames = Object.keys(genreCounts);
+
+  let chartData = [];
+
+  for (let i = 0; i < genreNames.length; i++) {
+    chartData.push({
+      'name': genreNames[i],
+      'angle': genreCounts[genreNames[i]],
+      'color': '#89DAC1'
+    });
+  }
+
+  console.log(chartData)
+
+  return (
+    <RadialChart
+      colorType={'literal'}
+      colorDomain={[0, 100]}
+      colorRange={[0, 10]}
+      margin={{ top: 100 }}
+      getLabel={d => d.name}
+      data={chartData}
+      labelsRadiusMultiplier={1.1}
+      labelsStyle={{ fontSize: 12, fill: '#222' }}
+      showLabels
+      style={{ stroke: '#fff', strokeWidth: 2 }}
+      width={600}
+      height={600}
+    />
+  );
+}
