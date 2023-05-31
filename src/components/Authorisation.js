1b1c872fa50015ec6c6a4a8b22e24f40b1362bce
Ben <ben.oldham2003@gmail.com>
2023-05-31 17:22:40 +0100

    Update redirect URL for deployment

--- src/components/Authorisation.js
+++ src/components/Authorisation.js
@@ -2,7 +2,9 @@ const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
 const RESPONSE_TYPE = "token";
 const SCOPE = "user-top-read";
 const CLIENT_ID = "268fc0cf3a024f2a8b409bbdb8095567";
-const REDIRECT_URI = "http://localhost:3000/";
+
+//const REDIRECT_URI = "http://localhost:3000/";
+const REDIRECT_URI = "https://spotify-analysis-1.vercel.app/";
 
 export function handleAuthentication() {
   const hash = window.location.hash;
